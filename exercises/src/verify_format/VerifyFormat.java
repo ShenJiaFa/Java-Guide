216267c5a7d40528f4519779e0031a9ecc1ec203
ShenJiaFa <sjfdzh@gmail.com>
2022-03-29 23:09:49 +0800

    [Description]: Fix some details

--- exercises/src/verify_format/VerifyFormat.java
+++ exercises/src/verify_format/VerifyFormat.java
@@ -2,15 +2,20 @@ package verify_format;
 
 /**
  * Function: Format validation using regular expressions
- * <p>
+ *
  * Author: ShenJiaFa
  * Since: 2022/3/28-22:32
  */
 public class VerifyFormat {
     public static void main(String[] args) {
 
+        // Phone number
         String phone = "15722926571";
+
+        // email address
         String email = "sjfdzh@gmail.com";
+
+        // telephone number
         String tel = "025-888888";
 
         System.out.println(verifyPhone(phone));
@@ -18,16 +23,34 @@ public class VerifyFormat {
         System.out.println(verifyTel(tel));
     }
 
+    /**
+     * Verify telephone number
+     *
+     * @param tel Telephone number
+     * @return Verify result
+     */
     private static boolean verifyTel(String tel) {
         return tel.matches("0\\d{2,6}-?\\d{5,20}");
     }
 
+    /**
+     * Verify email address
+     *
+     * @param email Email address
+     * @return Verify result
+     */
     private static boolean verifyEmail(String email) {
 
         return email.matches("\\w{1,30}@[a-zA-Z0-9]{2,20}(\\.[a-zA-Z0-9]{2,20}){1,2}");
 
     }
 
+    /**
+     * Verify phone number
+     *
+     * @param phone Phone number
+     * @return Verify result
+     */
     private static boolean verifyPhone(String phone) {
 
         return phone.matches("1[3-9]\\d{9}");
