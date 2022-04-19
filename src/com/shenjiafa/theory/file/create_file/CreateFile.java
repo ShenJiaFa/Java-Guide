08c22c9bd1ed31fb37b7162f3ce8c51fce076fab
ShenJiaFa <sjfdzh@gmail.com>
2022-04-19 22:40:01 +0800

    [Description]: Add a theory content

---
+++ src/com/shenjiafa/theory/file/create_file/CreateFile.java
@@ -0,0 +1,21 @@
+package com.shenjiafa.theory.file.create_file;
+
+import java.io.File;
+
+/**
+ * Function: Create file
+ *
+ * @author ShenJiaFa
+ * @since 2022/4/17
+ */
+public class CreateFile {
+    public static void main(String[] args) {
+
+        // Create a file object
+        File photoFile = new File("C:/MyCode/file/photo.jpg");
+
+        // Print file size in bytes
+        System.out.println(photoFile.length());
+
+    }
+}
