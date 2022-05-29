87cca8c9c298ea38c2717d61155e7f12f5403150
ShenJiaFa <sjfdzh@gmail.com>
2022-05-29 21:33:21 +0800

    [Description]: Add a content

--- src/com/shenjiafa/theory/reflect/method/GetMethod.java
+++ src/com/shenjiafa/theory/reflect/method/GetMethod.java
@@ -2,6 +2,9 @@ package com.shenjiafa.theory.reflect.method;
 
 import org.junit.Test;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
 /**
  * Function: Get method
  *
@@ -12,11 +15,27 @@ public class GetMethod {
 
     @Test
     public void TestGetDeclaredMethods(){
-
-   }
+        Class dogClass=Dog.class;
+        Method[] methods = dogClass.getDeclaredMethods();
+        for (Method method : methods) {
+            System.out.println(method.getName()+" || Return type: "+method.getReturnType()+"|| Parameter count: "+method.getParameterCount());
+        }
+    }
 
     @Test
-    public void TestGetDeclaredMethod(){
+    public void TestGetDeclaredMethod() throws NoSuchMethodException, InvocationTargetException, IllegalAccessException {
+
+        Class dogClass=Dog.class;
+        Method method1 = dogClass.getDeclaredMethod("eat");
+        Method method2 = dogClass.getDeclaredMethod("eat",String.class);
+
+        // Invoke method1 of dog
+        Dog dog=new Dog();
+        method1.invoke(dog);
+
+        // Invoke method2 of dog
+        Object res = method2.invoke(dog, "Apple");
+        System.out.println(res);
 
     }
 
